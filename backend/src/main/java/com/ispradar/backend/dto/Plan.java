7961f49360b40c01bf2826bfe24a9a07c7d65e71
0x46617465 <efstrmav@gmail.com>
2026-05-20 13:49:33 +0300

    Import List namespace for Plan dto

--- backend/src/main/java/com/ispradar/backend/dto/Plan.java
+++ backend/src/main/java/com/ispradar/backend/dto/Plan.java
@@ -1,10 +1,12 @@
 package com.ispradar.backend.dto;
 
+import java.util.List;
+
 public record Plan(
         String provider, // COSMOTE, VODAFONE, NOVA
         String name,     // plan name, like "COSMOTE 100 Mbps"
         Double maxDownloadMbps,
         Double maxUploadMbps,
         Double price,
-        java.util.List<String> description
+        List<String> description
 ) {}
